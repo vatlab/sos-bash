61ab4c983a2eda3d57426ee750eb7ebe1307b9d3
Bo Peng <ben.bog@gmail.com>
2017-10-05 08:24:01 -0500

    Add SoS Notebook extension for shell scripts, separated from SoS 0.9.9.1

---
+++ setup.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+#
+# This file is part of Script of Scripts (sos), a workflow system
+# for the execution of commands and scripts in different languages.
+# Please visit https://github.com/vatlab/SOS for more information.
+#
+# Copyright (C) 2016 Bo Peng (bpeng@mdanderson.org)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+import sys, os
+import shutil
+from setuptools import find_packages, setup
+from distutils import log
+from setuptools.command.install import install
+
+# obtain version of SoS
+with open('src/sos_bash/_version.py') as version:
+    for line in version:
+        if line.startswith('__version__'):
+            __version__ = eval(line.split('=')[1])
+            break
+
+setup(name = "sos-bash",
+    version = __version__,
+    description = 'batch execusion actions and bash kernel extension for sos',
+    author = 'Bo Peng',
+    url = 'https://github.com/vatlab/SOS',
+    author_email = 'bpeng@mdanderson.org',
+    maintainer = 'Bo Peng',
+    maintainer_email = 'bpeng@mdanderson.org',
+    license = 'GPL3',
+    include_package_data = True,
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Natural Language :: English',
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: Science/Research',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: Implementation :: CPython',
+        ],
+    packages = find_packages('src'),
+    package_dir = {'': 'src'},
+    install_requires=[
+          'sos-core',
+          'sos-notebook',
+      ],
+    entry_points= '''
+[sos_actions]
+bash = sos_bash.actions:bash
+csh = sos_bash.actions:csh
+tcsh = sos_bash.actions:tcsh
+zsh = sos_bash.actions:zsh
+sh = sos_bash.actions:sh
+
+[sos_languages]
+Bash = sos_bash.kernel:sos_Bash
+'''
+)
