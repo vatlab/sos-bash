c1c0b581d5864e20506aada152a21c6d871f5364
Bo Peng <ben.bog@gmail.com>
2017-11-12 10:23:48 -0600

    Release sos-bash 0.9.10.1

--- setup.py
+++ setup.py
@@ -59,8 +59,8 @@ setup(name = "sos-bash",
     packages = find_packages('src'),
     package_dir = {'': 'src'},
     install_requires=[
-          'sos>=0.9.10.1',
-          'sos-notebook',
+          'sos>=0.9.10.11',
+          'sos-notebook>=0.9.10.9',
           'bash-kernel',
       ],
     entry_points= '''
